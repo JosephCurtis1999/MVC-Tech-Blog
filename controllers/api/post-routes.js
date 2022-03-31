f65f72d904dd9dc9296769c851ad0e9159d6de5e
Joe Curtis <joecurtis9999@gmail.com>
2022-03-31 15:40:57 +0100

    post routes update post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { Post, Comment, User } = require("../../models/");
 const withAuth = require("../../utils/auth");
 
+// create post
 router.post("/", withAuth, (req, res) => {
     const body = req.body;
     console.log(req.session.userId);
@@ -14,3 +15,22 @@ router.post("/", withAuth, (req, res) => {
     });
 });
 
+// update post
+router.put("/:id", withAuth, (req,res) => {
+    console.log(req.body, req.params,id)
+    Post.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(affectedRows => {
+        if (affectedRows > 0) {
+            res.status(200).end();
+        } else {
+            res.status(404).end();
+        }
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
