0ce824f6afe8ae2b84075d96317f5aa608d4f96b
Joe Curtis <joecurtis9999@gmail.com>
2022-03-31 13:10:15 +0100

    finished server.js file

--- server.js
+++ server.js
@@ -29,3 +29,17 @@ const hbs = exphbs.create({
     }
 });
 
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(require('./controllers/'));
+
+app.listen(PORT, () => {
+    console.log('App listening on port ${PORT}!');
+    sequelize.sync({ force: false });
+});
+
