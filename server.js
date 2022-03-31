70b5596035480b8c4596bd1b3d4eee97092b3ebb
Joe Curtis <joecurtis9999@gmail.com>
2022-03-31 13:06:26 +0100

    server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require9("./config/connection.js")
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: "Super secret secret",
+    cookie: {},
+    resave: false,
+    saveUnitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({
+    helpers: {
+        format_date: date => {
+            return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+        }
+    }
+});
+
