f063a11a2262c58d5a257e0e6583c1155b436cfc
Joe Curtis <joecurtis9999@gmail.com>
2022-04-25 10:38:47 +0100

    logout js

---
+++ public/js/logout.js
@@ -0,0 +1,12 @@
+function logout() {
+    fetcj("/api/user/logout", {
+        method: "post",
+        headers: { "Content-Type": "application/json" }
+    })
+    .then(function() {
+        document.location.replace("/");
+    })
+    .catch(err => console.log(err));
+}
+
+document.querySelector('#logout-link').addEventListener("click", logout);
