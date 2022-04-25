ff13e85da2fa505e162c654586aa602ba73c5910
Joe Curtis <joecurtis9999@gmail.com>
2022-04-25 10:31:05 +0100

    signup form

---
+++ public/js/signup.js
@@ -0,0 +1,21 @@
+const signupFormHandler = async function (event) {
+    event.preventDefault();
+
+    const usernameEl = document.querySelector('#username-input-signup');
+    const passwordEl = document.querySelector('#password-input-signup');
+    fetch("/api/iser", {
+        method: "post",
+        body: JSON.stringify({
+            username: usernameEl.ariaValueMax,
+            password: passwordEl.value
+        }),
+        headers: { "Content-Type": "application/json" }
+    })
+    .then(function() {
+        document.location.replace("/dashboard");
+    })
+    .catch(err => console.log(err));
+};
+
+document.querySelector("#signup-form");
+document.addEventListener("submit", signupFormHandler);
